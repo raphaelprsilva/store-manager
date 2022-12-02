aeba2c021ebb6aa703b1c5b5b1220a3df8f51e74
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-02 09:55:49 -0300

    feat: add route GET `/products`

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const { productsRouter } = require('./routes');
 
 const app = express();
+app.use(bodyParser.json());
 
-// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+app.use('/products', productsRouter);
+
 module.exports = app;
