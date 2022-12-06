54cb4244d0ab69b9ec593f74f30f692ca8fb12d5
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-06 08:27:11 -0300

    test: implements `getById` controller layer

--- tests/unit/controllers/products.controllers.test.js
+++ tests/unit/controllers/products.controllers.test.js
@@ -48,4 +48,54 @@ describe('productsController unit tests', function () {
       });
     });
   });
+
+  describe('getById', function () {
+    describe('When product exists', async function () {
+      it('should return a response with status 200 and product data', async function () {
+        const req = {};
+        const res = {};
+
+        req.params = sinon.stub().returns(1);
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+
+        sinon.stub(productsService, 'getById').resolves({
+          type: null,
+          message: productsMock[0],
+        });
+
+        await productsController.getById(req, res);
+
+        expect(res.status).to.have.been.calledWith(200);
+        expect(res.json).to.have.been.calledWith(productsMock[0]);
+      });
+    });
+
+    describe('When product does not exists', async function () {
+      it('should return a response with status 404 and message indicating that', async function () {
+        const req = {};
+        const res = {};
+
+        req.params = sinon.stub().returns(45230);
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+
+        sinon.stub(productsService, 'getById').resolves({
+          type: 'notFound',
+          message: 'Product not found',
+        });
+
+        await productsController.getById(req, res);
+
+        expect(res.status).to.have.been.calledWith(404);
+        expect(res.json).to.have.been.calledWith({
+          message: 'Product not found',
+        });
+      });
+    });
+
+    afterEach(function () {
+      sinon.restore();
+    });
+  });
 });
