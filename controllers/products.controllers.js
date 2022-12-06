8085161ab305f181da45f5213750df74cc2b99b4
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-06 08:27:22 -0300

    feat: implements `getById` controller layer

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -6,6 +6,18 @@ const getAll = async (_req, res) => {
   return res.status(200).json(products);
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await productsService.getById(id);
+
+  if (type) {
+    return res.status(404).json({ message });
+  }
+
+  return res.status(200).json(message);
+};
+
 module.exports = {
   getAll,
+  getById,
 };
