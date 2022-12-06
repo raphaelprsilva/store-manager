14e753230f3580e0f10b5df63aefdff9046f02ff
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-06 08:52:08 -0300

    test: implements `getById` model layer

--- tests/unit/models/products.models.test.js
+++ tests/unit/models/products.models.test.js
@@ -31,6 +31,30 @@ describe('productsModel unit tests', function () {
     });
   });
 
+  describe('getById', function () {
+    describe('When product does exists', function () {
+      it('should return an array with product data', async function () {
+        sinon.stub(connection, 'execute').resolves([[productsMock[0]]]);
+
+        const product = await productsModel.getById(1);
+
+        expect(product).to.be.an('array');
+        expect(product).to.have.lengthOf(1);
+      });
+    });
+
+    describe('When product does not exists', function () {
+      it('should return an array with product data', async function () {
+        sinon.stub(connection, 'execute').resolves([[]]);
+
+        const product = await productsModel.getById(644165);
+
+        expect(product).to.be.an('array');
+        expect(product).to.have.lengthOf(0);
+      });
+    });
+  });
+
   afterEach(function () {
     sinon.restore();
   });
