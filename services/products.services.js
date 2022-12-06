3f53fb2e490f4a7ba201da9d6f1ea3170534795b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-06 07:59:08 -0300

    feat: implements `getById` function tests

--- services/products.services.js
+++ services/products.services.js
@@ -5,6 +5,17 @@ const getAll = async () => {
   return products;
 };
 
+const getById = async (id) => {
+  const [product] = await productsModel.getById(id);
+
+  if (!product) {
+    return { type: 'notFound', message: 'Product not found' };
+  }
+
+  return { type: null, message: product };
+};
+
 module.exports = {
   getAll,
+  getById,
 };
