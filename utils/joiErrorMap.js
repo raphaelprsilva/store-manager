6ca99210213915d5802f2abe92ee6d8273404c9e
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-08 10:33:44 -0300

    feat: update joiErrorMap

--- utils/joiErrorMap.js
+++ utils/joiErrorMap.js
@@ -2,9 +2,10 @@ const joiErrorStatus = {
   'any.required': 'BAD_REQUEST',
   'string.min': 'INVALID_VALUE',
   'number.base': 'BAD_REQUEST',
-  'number.min': 'BAD_REQUEST',
+  'number.min': 'INVALID_VALUE',
   'string.base': 'BAD_REQUEST',
   'string.empty': 'INVALID_VALUE',
+  'number.empty': 'INVALID_VALUE',
 };
 
 module.exports = {
