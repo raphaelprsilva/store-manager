4eeb4b92bbc836a787c91981f3df7b7dac7bebe3
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-08 13:06:05 -0300

    test: add mocks

--- tests/unit/mocks/sales.mock.js
+++ tests/unit/mocks/sales.mock.js
@@ -13,9 +13,41 @@ const salesMock = [
   },
 ];
 
+const salesMockNotSorted = [
+  {
+    saleId: 2,
+    date: '2021-09-01T00:00:00.000Z',
+    productId: 2,
+    quantity: 2,
+  },
+  {
+    saleId: 1,
+    date: '2021-09-01T00:00:00.000Z',
+    productId: 1,
+    quantity: 1,
+  },
+];
+
+const salesMockSorted = [
+  {
+    saleId: 1,
+    date: '2021-09-01T00:00:00.000Z',
+    productId: 1,
+    quantity: 1,
+  },
+  {
+    saleId: 2,
+    date: '2021-09-01T00:00:00.000Z',
+    productId: 2,
+    quantity: 2,
+  },
+];
+
 const emptySalesMock = [];
 
 module.exports = {
   salesMock,
   emptySalesMock,
+  salesMockNotSorted,
+  salesMockSorted,
 };
