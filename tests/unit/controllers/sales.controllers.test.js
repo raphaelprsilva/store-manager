d7148fa0861e6588955d1ad0a394fc402ef9fc10
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-08 16:08:15 -0300

    test: implements `getById` at `sales` controller layer

--- tests/unit/controllers/sales.controllers.test.js
+++ tests/unit/controllers/sales.controllers.test.js
@@ -117,4 +117,66 @@ describe('salesController unit tests', function () {
       sinon.restore();
     });
   });
+
+  describe('getById', function () {
+    it('should return a response with status 200 and a JSON with the sale', async function () {
+      const req = {};
+      const res = {};
+
+      req.params = { id: 1 };
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon.stub(salesService, 'getById').resolves({
+        type: null,
+        message: [
+          {
+            date: '2021-09-01T00:00:00.000Z',
+            productId: 1,
+            quantity: 2,
+          },
+        ],
+      });
+
+      await salesController.getById(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith([
+        {
+          date: '2021-09-01T00:00:00.000Z',
+          productId: 1,
+          quantity: 2,
+        },
+      ]);
+
+      sinon.restore();
+    });
+
+    it('should return a response with status 404 and a JSON with the error message', async function () {
+      const req = {};
+      const res = {};
+
+      req.params = { id: 42 };
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon.stub(salesService, 'getById').resolves({
+        type: 'SALE_NOT_FOUND',
+        message: 'Sale not found',
+      });
+
+      await salesController.getById(req, res);
+
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWith({
+        message: 'Sale not found',
+      });
+
+      sinon.restore();
+    });
+
+    afterEach(function () {
+      sinon.restore();
+    });
+  });
 });
