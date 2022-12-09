f1a703c1fcc3acbc9332a975b114f46c4f0198ac
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-09 15:23:29 -0300

    test: implements `update` at `sales` controllers layer

--- tests/unit/controllers/sales.controllers.test.js
+++ tests/unit/controllers/sales.controllers.test.js
@@ -179,4 +179,72 @@ describe('salesController unit tests', function () {
       sinon.restore();
     });
   });
+
+  describe('update', function () {
+    it('should return a response with status 200 and a JSON with the updated sale', async function () {
+      const req = {};
+      const res = {};
+
+      req.params = { id: 1 };
+      req.body = sinon.stub().returns({});
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon.stub(salesService, 'update').resolves({
+        type: null,
+        message: {
+          id: 1,
+          itensSold: [
+            {
+              productId: 1,
+              quantity: 2,
+            },
+          ],
+        },
+      });
+
+      await salesController.update(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith({
+        id: 1,
+        itensSold: [
+          {
+            productId: 1,
+            quantity: 2,
+          },
+        ],
+      });
+
+      sinon.restore();
+    });
+
+    it('should return a response with status 404 and a JSON with the error message', async function () {
+      const req = {};
+      const res = {};
+
+      req.params = { id: 42 };
+      req.body = sinon.stub().returns({});
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon.stub(salesService, 'update').resolves({
+        type: 'SALE_NOT_FOUND',
+        message: 'Sale not found',
+      });
+
+      await salesController.update(req, res);
+
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWith({
+        message: 'Sale not found',
+      });
+
+      sinon.restore();
+    });
+
+    afterEach(function () {
+      sinon.restore();
+    });
+  });
 });
