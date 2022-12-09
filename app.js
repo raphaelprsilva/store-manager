0d857381c6cfd43aeccd8f2dc9f0eda7ca1a6366
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-12-09 15:45:38 -0300

    feat: add route `/api-docs`

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
 
 const { productsRouter, salesRouter } = require('./routes');
+
+const swaggerFile = require('./swagger-output.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,6 +15,14 @@ app.get('/', (_request, response) => {
 
 app.use('/products', productsRouter);
 app.use('/sales', salesRouter);
+
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerFile, {
+    swaggerOptions: { persistAuthorization: true },
+  }),
+);
 
 app.use('/*', (_request, response) => {
   response.status(404).json({ message: 'Not found' });
